0ddbd9e0cf48b7ddedaa47abc294b65d6370c829
Miles Elliott <me@mileselliott.me>
2020-06-20 04:35:38 -0400

    more better

--- src/components/selectionMenu.svelte
+++ src/components/selectionMenu.svelte
@@ -2,17 +2,35 @@
 import drinks from '../drinks.json'
 export let setView;
 const getRandom = () => drinks[Math.floor(Math.random() * (0, drinks.length))]
-let showRandomCats = false;
 </script>
 
-<div>
-    <button on:click={setView('drink', {drink: getRandom()})}>Get Random</button>
-    <button on:click={setView('drinkList', {drinks})}>Browse</button>
-</div>
+<style>
+div {
+    position: fixed;
+    bottom: 0;
+    left: 0;
+    width: 100%;
+    display: flex;
+}
+button {
+    flex: 1 1 auto;
+    background: white;
+    color: purple;
+    border: solid 2px purple;
+    margin: 0;
+    cursor: pointer;
+}
+button:hover {
+    color: white;
+    background: purple;
+}
+button:first-child {
+    border-right: none;
+}
+</style>
 
-{#if showRandomCats}
 <div>
-<button on:click={setView('drink', {drink: getRandom()})}>Get Random</button>
+    <button on:click={setView('drinkList', {drinks})}>Browse</button>
+    <button on:click={setView('drink', {drink: getRandom()})}>Get Random</button>
 </div>
-{/if}
 
